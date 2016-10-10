060528bbf9c4a2e4ec98b2535c5fd728707a8fa1
abelmagana88920 <abelmagana88920@gmail.com>
2016-10-10 17:37:36 +0800

    add modal to material

--- app/components/material/material.js
+++ app/components/material/material.js
@@ -1,6 +1,25 @@
 
           angular.module('app')
-            .controller('myAppController', ['$scope', 'myAppFactory', function ($scope, myAppFactory) {
+            .controller('myAppController', ['$scope', 'myAppFactory','$state','ModalService', 'QueryService','logger', function ($scope, myAppFactory,$state,ModalService, QueryService,logger) {
+
+
+                var vm = this;
+        
+                vm.titleHeader = 'Material';
+                vm.message_modal = message_modal;
+                vm.data = {};
+
+                
+                function message_modal () {
+                    var content =  {
+                        header:'Message',
+                        message:'Hello World!'
+                    }
+                    ModalService.confirm_modal(content);
+                }
+
+
+
 
                 $scope.gridOptions = {
                     data: [],
